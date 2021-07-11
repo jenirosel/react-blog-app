7b54aec75a102bd46edab4a262aaf601a91f624b
“rosel.jeni” <rosel.jeni@gmail.com>
2021-07-12 01:18:18 +0900

    Add API CRUD functionality

---
+++ api/routes/categories.js
@@ -0,0 +1,25 @@
+const router = require("express").Router();
+const Category = require("../models/Category");
+
+
+router.post("/", async(req,res) => {
+  const newCat = new Category(req.body);
+  try {
+    const savedCat = await newCat.save();
+    res.status(200).json(savedCat);
+  } catch(err) {
+    res.status(500).json(err);
+  }
+})
+
+router.get("/", async(req, res) => {
+  try {
+    const categories = await Category.find();
+    res.status(200).json(categories);
+  } catch {
+    res.status(500).json(er);
+  }
+})
+
+
+module.exports = router;
